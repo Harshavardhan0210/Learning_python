a5cc04c27eeeb8ccfc58870d757a440120cb01a6
Harshavardhan M M <mmharshavardhan5@gmail.com>
2026-04-15 23:03:42 +0530

    Add files via upload

---
+++ guess-game.py
@@ -0,0 +1,19 @@
+import random
+
+number = random.randint(1,100)
+attempts = 0
+
+print("Guess the number between 1 to 100")
+
+while True:
+    guess=int(input("Enter your Guess :"))
+    attempts += 1
+
+    if guess > number :
+        print("Too High")
+    elif guess < number:
+        print("Too Low")
+    else:
+        print("Correct !!")
+        print("No of Attempts :",attempts)
+        break
